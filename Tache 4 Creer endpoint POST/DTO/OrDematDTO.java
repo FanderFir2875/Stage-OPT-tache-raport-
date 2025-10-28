c60cabca87039605821c46cadb3cdd09afaa425f
FanderFir2875 <168529036+FanderFir2875@users.noreply.github.com>
2025-10-28 11:58:47 +1100

    integration Lombok

--- Tache 4 Creer endpoint POST/DTO/OrDematDTO.java
+++ Tache 4 Creer endpoint POST/DTO/OrDematDTO.java
@@ -1,31 +1,17 @@
 package nc.opt.sior.service.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrDematDTO {
+
     private String DateDebut;
     private String DateFin;
-
-    public OrDematDTO() {}
-
-    public OrDematDTO(String DateDebut,String DateFin){
-        this.DateDebut= DateDebut;
-        this.DateFin = DateFin;
-    }
-
-    public String getDateDebut(){
-        return DateDebut;
-    }
-
-    public void setDateDebut(String DateDebut){
-        this.DateDebut = DateDebut;
-    }
-
-    public String getDateFin(){
-        return DateFin;
-    }
-
-    public void setDateFin(String DateFin){
-        this.DateFin = DateFin;
-    }
 
 
 }
